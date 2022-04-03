02708ba6cbb5c7666f75038dbd534956010afa9a
Krystian Grela <krystiangrela9@gmail.com>
2022-04-03 23:32:45 +0200

    final version of allegro test

--- test/specs/allegro.test.js
+++ test/specs/allegro.test.js
@@ -1,9 +1,10 @@
 describe("Allegro - purchase", () => {
-	it("should redirect to Allegro page", async () => {
+	it("should see cookies part and redirect to Allegro page", async () => {
 		browser.setWindowSize(1920, 1080)
 		await browser.url(`https://allegro.pl/`)
 		await browser.pause(1000)
 		const cookiesBttn = $('[data-role="accept-consent"]')
+		expect(cookiesBttn).toBeDisplayed()
 		await cookiesBttn.click()
 		await browser.pause(1000)
 	})
@@ -11,6 +12,8 @@ describe("Allegro - purchase", () => {
 	it("should choose Electronic option", async () => {
 		const electronicsOption = $('[data-group-id="departments_Elektronika"]')
 		await electronicsOption.click()
+		const filtersSelectedOption = $('[data-role="filters-selected-option"]')
+		await expect(filtersSelectedOption).toHaveText("Elektronika")
 	})
 
 	it("should choose More Categories option", async () => {
@@ -18,6 +21,9 @@ describe("Allegro - purchase", () => {
 			'[data-analytics-view-value="Więcej kategorii"]'
 		)
 		await moreCategoriesOption.click()
+		await expect(
+			$('[data-box-name="Kategorie modal on.default"]')
+		).toBeExisting()
 	})
 
 	it("should choose Headphones option from the list", async () => {
@@ -28,6 +34,8 @@ describe("Allegro - purchase", () => {
 		await browser.switchWindow(
 			"Słuchawki - RTV i AGD - Elektronika - Allegro.pl"
 		)
+		const filtersSelectedOption = $('[data-role="filters-selected-option"]')
+		await expect(filtersSelectedOption).toHaveText("Słuchawki")
 	})
 
 	it("should display the list in term of popularity sorting ", async () => {
@@ -36,12 +44,17 @@ describe("Allegro - purchase", () => {
 			"data-event-id",
 			"POPULARITY_DESC"
 		)
+		await expect(
+			$('select[id="allegro.listing.sort"] [data-event-id="POPULARITY_DESC"]')
+		).toHaveAttribute("selected", "popularność: największa")
 	})
 
 	it("should choose the first item from the list", async () => {
 		const chooseFirstRecord = $('[class="opbox-listing"] article:first-child')
 		await chooseFirstRecord.click()
 		await browser.pause(2000)
+		const filtersSelectedOption = $('[data-role="filters-selected-option"]')
+		await expect(filtersSelectedOption).toHaveText("Bezprzewodowe")
 		// let productName1 = $(
 		// 	'[data-role="app-container"] h1:first-of-type'
 		// ).getText()
@@ -50,6 +63,10 @@ describe("Allegro - purchase", () => {
 	it("should add an item to the cart", async () => {
 		const addToCartBttn = $('button[id="add-to-cart-button"]')
 		await addToCartBttn.click()
+		const addedItemInfo = $(
+			'[data-box-name="precart-layer-title-offer"] div :first-child'
+		)
+		await expect(addedItemInfo).toHaveText("Dodałeś przedmiot do koszyka")
 	})
 
 	it("should click on the button responsible for continuing purchase", async () => {
@@ -62,17 +79,22 @@ describe("Allegro - purchase", () => {
 	it("should go to Home Page", async () => {
 		const goToHomePage = $('[data-analytics-click-value="logotype"]')
 		await goToHomePage.click()
+		const filtersSelectedOption = $('[data-role="filters-selected-option"]')
+		await expect(filtersSelectedOption).toHaveText("Wszystkie kategorie")
 	})
 
 	it("should type value in search field", async () => {
 		const searchInput = $('[data-role="search-input"]')
 		await searchInput.setValue("długopis 3d")
+		expect(searchInput).toBeDisplayed("długopis 3d")
 	})
 
 	it("should click on Search button", async () => {
 		const searchBttn = $('[data-role="search-button"]')
 		await searchBttn.click()
 		await browser.pause(2000)
+		const listingTitle = $('[data-box-name="Listing title"] div :nth-child(2)')
+		await expect(listingTitle).toHaveText("długopis 3d")
 	})
 
 	it("should provide values in price range", async () => {
@@ -83,6 +105,8 @@ describe("Allegro - purchase", () => {
 		const priceRangeTo = $('[id="price_to"]')
 		await priceRangeTo.setValue("500")
 		await browser.pause(1000)
+		expect(priceRangeFrom).toBeDisplayed("250")
+		expect(priceRangeTo).toBeDisplayed("500")
 		const chooseFirstRecord = $('[class="opbox-listing"] article:first-child')
 		await chooseFirstRecord.scrollIntoView()
 	})
@@ -91,6 +115,8 @@ describe("Allegro - purchase", () => {
 		const chooseFirstRecord = $('[class="opbox-listing"] article:first-child')
 		await chooseFirstRecord.click()
 		await browser.pause(2000)
+		const filtersSelectedOption = $('[data-role="filters-selected-option"]')
+		await expect(filtersSelectedOption).toHaveText("Urządzenia")
 		// let productName2 = $(
 		// 	'[data-role="app-container"] h1:first-of-type'
 		// ).getText()
@@ -103,10 +129,18 @@ describe("Allegro - purchase", () => {
 	it("should add an item to the cart", async () => {
 		const addToCartBttn = $('button[id="add-to-cart-button"]')
 		await addToCartBttn.click()
+		const addedItemInfo = $(
+			'[data-box-name="precart-layer-title-offer"] div :first-child'
+		)
+		await expect(addedItemInfo).toHaveText("Dodałeś przedmiot do koszyka")
 	})
 
 	it("should go to My Cart view from the item popup", async () => {
 		const goToCartModalBttn = $('[data-analytics-click-label="goToCart"]')
 		await goToCartModalBttn.click()
+		const deliveryAndPaymentBttn = $(
+			'[data-analytics-view-label="deliveryAndPaymentButtonState"]'
+		)
+		await expect(deliveryAndPaymentBttn).toBeClickable()
 	})
 })
